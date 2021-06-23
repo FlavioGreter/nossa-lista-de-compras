c10c46ca4144a480db52936a678ead19f6d83f8e
Flavio Gretter <flaviogreter@gmail.com>
2021-06-23 19:00:51 -0400

    Initial Commit

---
+++ src/screens/home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, Text, View } from 'react-native';
+
+const Home = () => {
+    return(
+        <SafeAreaView>
+            <View>
+                <Text>Nossa lista de compras!</Text>
+            </View>
+        </SafeAreaView>
+        
+    )
+}
+export default Home;
+
